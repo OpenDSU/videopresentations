bcb9f8bba84209be191610e7587febe7371570ea
salboaie <salboaie@gmail.com>
2024-01-04 20:46:58 +0200

    new video projects

--- OpenDSU EPCIS/build.sh
+++ OpenDSU EPCIS/build.sh
@@ -1,4 +1,3 @@
-#node ../bin/text2mp3.js ./texts/S0 ./voice/S0.mp3
 #node ../bin/text2mp3.js ./texts/S1 ./voice/S1.mp3
 #node ../bin/text2mp3.js ./texts/S2 ./voice/S2.mp3
 #node ../bin/text2mp3.js ./texts/S3 ./voice/S3.mp3
@@ -9,15 +8,15 @@
 #node ../bin/text2mp3.js ./texts/S8 ./voice/S8.mp3
 #node ../bin/text2mp3.js ./texts/S9 ./voice/S9.mp3
 
-ffmpeg -loop 1 -i ./slides/S1.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 93 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S1.mp4
-ffmpeg -loop 1 -i ./slides/S2.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 123 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S2.mp4
-ffmpeg -loop 1 -i ./slides/S3.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 132 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S3.mp4
+#ffmpeg -loop 1 -i ./slides/S1.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 93 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S1.mp4
+#ffmpeg -loop 1 -i ./slides/S2.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 123 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S2.mp4
+#ffmpeg -loop 1 -i ./slides/S3.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 132 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S3.mp4
 ffmpeg -loop 1 -i ./slides/S4.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 130 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S4.mp4
-ffmpeg -loop 1 -i ./slides/S5.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 188 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S5.mp4
-ffmpeg -loop 1 -i ./slides/S6.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 140 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S6.mp4
-ffmpeg -loop 1 -i ./slides/S7.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 86 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S7.mp4
-ffmpeg -loop 1 -i ./slides/S8.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 64 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S8.mp4
-ffmpeg -loop 1 -i ./slides/S9.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 70 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S9.mp4
+#ffmpeg -loop 1 -i ./slides/S5.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 188 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S5.mp4
+#ffmpeg -loop 1 -i ./slides/S6.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 140 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S6.mp4
+#ffmpeg -loop 1 -i ./slides/S7.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 86 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S7.mp4
+#ffmpeg -loop 1 -i ./slides/S8.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 64 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S8.mp4
+#ffmpeg -loop 1 -i ./slides/S9.png  -vf "scale=960:540" -c:v libaom-av1 -crf 0 -t 70 -r 24 -pix_fmt yuva444p -b:v 4000k ./video/S9.mp4
 
 cd ./voice
 ffmpeg -f concat -safe 0 -i ./files -c copy ../temp/allsound.mp3
